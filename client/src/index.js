4b10a0231e25d1adcacf725d126ea73b766dff80
ananya92 <1992ananya@gmail.com>
2020-05-28 17:03:44 +0930

    Implemented patient search bar to search by name or phone number using react-bootstrap-typeahead

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-bootstrap-typeahead/css/Typeahead.css';
 
 ReactDOM.render(
     <BrowserRouter>
