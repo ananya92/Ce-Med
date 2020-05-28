eaacced6795727f01a333ae8fbe474a6266a10a9
ananya92 <1992ananya@gmail.com>
2020-05-28 20:05:58 +0930

    Fixing heroku issue - unable to load logo from the public directory. Moved it inside src

--- client/src/components/navbar/index.js
+++ client/src/components/navbar/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
-
+import logo from './img/logo.png';
 function MedNavbar() {
     return (
         <Navbar>
             <Navbar.Brand href="/">
                 <img
-                    src="/favicon.png"
+                    src={logo}
                     width="70"
                     height="45"
                     className="logo"
