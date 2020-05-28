0049d2cea6133c5df25933ca73cfa221a017e136
anu-dam <anuradha.dambaladeniya@gmail.com>
2020-05-28 15:56:23 +0930

    Login page allignments

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -3,23 +3,19 @@ import { Form, Button, Container } from "react-bootstrap";
 
 function Login() {
     return (
-
         <Container style={{ width: "50%", marginTop: 50 }}>
             <Form>
                 <Form.Group controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
                     <Form.Control type="email" placeholder="Enter email" />
-
                 </Form.Group>
-
                 <Form.Group controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
                     <Form.Control type="password" placeholder="Password" />
                 </Form.Group>
-
                 <Button variant="primary" type="submit">
                     Submit
-        </Button>
+                </Button>
             </Form>
         </Container>
     )
