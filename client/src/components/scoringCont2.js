fa079a5950ae014c54dc2919f7dcf5b00e8518ff
ananya92 <1992ananya@gmail.com>
2020-06-05 13:37:20 +0930

    Added tabs for the different forms in scoringCont2 page

--- client/src/components/scoringCont2.js
+++ client/src/components/scoringCont2.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Tabs, Tab } from 'react-bootstrap';
+import MedicationIntravenous from "./medicationIntravenous";
+import LaboratoryImbalances from "./laboratoryImbalances";
+import MonitoringInvasiveLines from "./monitoringInvasiveLines";
+import EmergencyProcedures from "./emergencyProcedures";
 
 function ScoringCont2(props) {
+    const [key, setKey] = useState('Medication Intravenous');
     return (
         <div>
             <p>Patient ID: {props.currentPatient.patientID} &emsp; Name: {props.currentPatient.name}</p>
-            <h2>Scoring Cont 2 Page</h2>
+            <Tabs
+                id="controlled-tab-example"
+                activeKey={key}
+                onSelect={(k) => setKey(k)}
+            >
+                <Tab eventKey="Medication Intravenous" title="Medication Intravenous">
+                    <MedicationIntravenous/>
+                </Tab>
+                <Tab eventKey="Laboratory Imbalances" title="Laboratory Imbalances">
+                    <LaboratoryImbalances/>
+                </Tab>
+                <Tab eventKey="Monitoring / Invasive Lines" title="Monitoring / Invasive Lines">
+                    <MonitoringInvasiveLines/>
+                </Tab>
+                <Tab eventKey="Emergency Procedures Within 24 hrs" title="Emergency Procedures Within 24 hrs">
+                    <EmergencyProcedures/>
+                </Tab>
+            </Tabs>
         </div>
     )
 }
